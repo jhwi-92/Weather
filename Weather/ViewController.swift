1f105961ae6847d88b8765398885f4395427145e
Weather <dwg100@nate.com>
2021-04-05 19:52:31 +0900

    1일차 런치스크린, 네비게이션바 투명도 설정 버튼아이템 추가, 메인 뷰 백그라운드 이미지 추가

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -10,10 +10,39 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.addBackground()
+        self.setNavigationBar()
+        
     }
 
+    func setNavigationBar(){
+            let bar:UINavigationBar! =  self.navigationController?.navigationBar
+        bar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+            bar.shadowImage = UIImage()
+        bar.backgroundColor = UIColor.clear
+        }
 
+
+    //출처: https://tom7930.tistory.com/23 [Dr.kim의 나를 위한 블로그]
 }
 
+
+
+extension UIView {
+func addBackground() {
+    // screen width and height:
+    let width = UIScreen.main.bounds.size.width
+    let height = UIScreen.main.bounds.size.height
+
+    let imageViewBackground = UIImageView(frame: CGRect(x: 0, y: 0, width: width, height: height))
+    imageViewBackground.image = UIImage(named: "backgroundIMG")
+
+    // you can change the content mode:
+    imageViewBackground.contentMode = UIView.ContentMode.scaleAspectFill
+
+    self.addSubview(imageViewBackground)
+    self.sendSubviewToBack(imageViewBackground)
+}}
